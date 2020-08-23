14e6989d4e7a432e732500b5ab77ea8cf26c1449
wf <2291844002@qq.com>
2020-08-23 23:31:48 +0800

    给切片追加元素-把切片中元素拆开放入另一个切片的底层数组中

--- day2/042slice_append&copy/main.go
+++ day2/042slice_append&copy/main.go
@@ -16,4 +16,14 @@ func main() {
 	fmt.Println(s1)
 	fmt.Printf("%d\n", cap(s1))
 
+	s1 = append(s1, "南京1")
+	fmt.Printf("%v,%d\n", s1, cap(s1))
+
+	s1 = append(s1, "南京2", "南京3")
+	fmt.Printf("%v,%d\n", s1, cap(s1))
+
+	//把切片ss中的元素拆开放入s1中
+	ss := []string{"武汉", "南京", "上海"}
+	s1 = append(s1, ss...) //...表示拆开
+	fmt.Printf("%v", s1)
 }
